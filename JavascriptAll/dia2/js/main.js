5e3b4e52c3e2defa5e7984c3d7f05cf0794aa2f1
Adria <adrivid@gmail.com>
2022-09-19 13:50:16 +0200

    ejercicios

--- JavascriptAll/dia2/js/main.js
+++ JavascriptAll/dia2/js/main.js
@@ -77,14 +77,123 @@
 // console.log("Bienvenido " + nom);
 
 //EJERCICIO 4
-let radio = parseFloat(prompt('Cual es el radio del circulo?? '));
-areaCirculo(radio);
+// let radio = parseFloat(prompt('Cual es el radio del circulo?? '));
+// areaCirculo(radio);
 
-function areaCirculo(r) {
-  const pi = Math.PI;
+// function areaCirculo(r) {
+//   const pi = Math.PI;
 
-  let resultado = pi * (r ** 2);
-  console.log('El area es ' + resultado);
+//   let resultado = pi * (r ** 2);
+//   console.log('El area es ' + resultado);
 
-  return resultado;
+//   return resultado;
+// }
+
+//EJERCICIO 5
+
+// let num = parseFloat(prompt('Dime un numero?? '));
+
+// if (num % 2 == 0) {
+//   console.log(num + ' es divisible entre 2');
+// } else {
+//   console.log(num + ' No es divisible entre 2');
+// }
+
+//EJERCICIO 6
+//  let num = parseFloat(prompt('Dime precio del producto?? '));
+//  console.log("el precio + IVA es: "+ num * 1.21);
+
+//EJERCICIO 9
+// let i = 1;
+// while(i < 101){
+//   if(i % 2 == 0 && i % 3 == 0){
+//     console.log(i);
+//   }
+// }
+
+//EJERCICIO 10
+/* 
+Realiza una aplicación que nos pida un número de ventas a introducir, después
+nos pedirá tantas ventas por teclado como número de ventas se hayan indicado.
+Al final mostrara la suma de todas las ventas. Piensa que es lo que se repite y lo
+que no.*/
+
+// let numVentas = parseInt(prompt('Introduce el numero de ventas: '));
+// let ventasTotal = 0;
+// for (let index = 1; index <= numVentas; index++) {
+//   ventasTotal += parseInt(prompt('Introduce el precio de ventas: '));
+//   console.log(ventasTotal);
+// }
+
+//EJERCICIO 11
+// let day = prompt('Introduce dia de la semana: ').toLowerCase();
+
+// switch (day) {
+//   case 'lunes':
+//     console.log('Es laboral');
+//     break;
+//   case 'martes':
+//     console.log('Es laboral');
+
+//     break;
+//   case 'miercoles':
+//     console.log('Es laboral');
+
+//     break;
+//   case 'jueves':
+//     console.log('Es laboral');
+
+//     break;
+//   case 'viernes':
+//     console.log('Es laboral');
+
+//     break;
+//   case 'sabado':
+//     console.log('No es laboral');
+//     break;
+
+//   case 'domingo':
+//     console.log('No es laboral');
+//     break;
+//   default:
+//     console.log('Te has equivocado');
+
+// code block
+//}
+
+//EJERCICIO 12
+// let password = 'hola';
+// let intentos = 1;
+
+// while (intentos < 3) {
+//   let userPassword = prompt('introduce contraseña');
+
+//   if (userPassword == password) {
+//     prompt('Enhorabuena');
+//   } else {
+//     let userPassword = prompt('introduce contraseña');
+//     intentos++;
+//   }
+// }
+// if ((intentos = 3)) {
+//   prompt('Te has quedado sin intentos');
+// }
+
+//EJERCICIO 13
+
+let n1 = parseInt(prompt('Introduce el primer numero: '));
+let n2 = parseInt(prompt('Introduce el primer numero: '));
+let operador = parseInt(prompt('Introduce el operador: ( + , - , * , / , %) '));
+
+calculadora(n1, n2, operador);
+function calculadora(n1, n2, operador) {
+  switch (operador) {
+    case '+':
+      console.log(n1 + n2);
+      console.log(n1 + ' + ' + n2 + ' es: ' + n1 + n2);
+      break;
+
+    default:
+      break;
+  }
 }
